7dcab80c3c9834257571f198dbb6a1bdb74117cd
장근영 <jky00914@naver.com>
2023-11-18 13:05:55 +0900

    test코드 수정

--- src/main/java/com/example/project3/service/S3Uploader.java
+++ src/main/java/com/example/project3/service/S3Uploader.java
@@ -2,6 +2,7 @@ package com.example.project3.service;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
@@ -111,6 +112,10 @@ public class S3Uploader {
             e.printStackTrace();
             return Optional.empty();
         }
+    }
+
+    public void deleteFile(String fileName) {
+        amazonS3Client.deleteObject(new DeleteObjectRequest(bucketName, fileName));
     }
 
 
