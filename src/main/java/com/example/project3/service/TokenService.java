3138ae396370d411f90b98fdfc69699aabf0a480
장근영 <jky00914@naver.com>
2023-11-26 15:02:12 +0900

    소셜유저 updateRefreshToken이 안되던 오류 수정

--- src/main/java/com/example/project3/service/TokenService.java
+++ src/main/java/com/example/project3/service/TokenService.java
@@ -6,6 +6,7 @@ import com.example.project3.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletResponse;
