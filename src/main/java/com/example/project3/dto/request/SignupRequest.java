7ce7c7707276ddb85de034c3ae16f87d56e14ae3
장근영 <jky00914@naver.com>
2023-11-28 18:16:47 +0900

    Swagger 문서 정리 - MemberController API - DTO - Swagger에서 헤더에 토큰 요청할 수 있게 추가

--- src/main/java/com/example/project3/dto/request/SignupRequest.java
+++ src/main/java/com/example/project3/dto/request/SignupRequest.java
@@ -1,5 +1,7 @@
 package com.example.project3.dto.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -8,15 +10,19 @@ import javax.validation.constraints.Pattern;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ApiModel(value = "회원가입 요청 정보")
 public class SignupRequest extends UpdateUserInfoRequest{
 
+    @ApiModelProperty(value = "이름", required = true, example = "사용자1")
     @NotBlank(message = "Username is required") // "", " ", null 허용 안함.
     private String userName;
 
+    @ApiModelProperty(value = "이메일(유효성 검사 : 이메일 형식)", required = true, example = "test@email.com")
     @NotBlank(message = "Email is required") // "", " ", null 허용 안함.
     @Email(message = "이메일 형식을 맞춰주세요.")
     private String email;
 
+    @ApiModelProperty(value = "비밀번호(유효성 검사 : 8 ~ 20자, 최소 한개의 특수문자와 숫자, 영문 알파벳을 포함)", required = true, example = "password12@")
     @NotBlank(message = "Password is required")
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[!@#$%^&*?])(?=.*[a-zA-Z]).{8,20}$",
             message = "8 ~ 20자, 최소 한개의 특수문자와 숫자, 영문 알파벳을 포함해야 함.")
--- src/main/java/com/example/project3/dto/request/UpdateUserInfoRequest.java
+++ src/main/java/com/example/project3/dto/request/UpdateUserInfoRequest.java
@@ -1,5 +1,7 @@
 package com.example.project3.dto.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,11 +10,15 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ApiModel(value = "회원 정보 수정 요청 정보")
 public class UpdateUserInfoRequest {
 
+    @ApiModelProperty(value = "회원 주소" , example = "서울특별시")
     private String address;
 
+    @ApiModelProperty(value = "회원 별명" , example = "별명1")
     private String nickName;
 
+    @ApiModelProperty(value = "회원 한 줄 메시지" , example = "날씨가 좋습니다.")
     private String message;
 }
