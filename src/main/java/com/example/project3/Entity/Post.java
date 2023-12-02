772d776bce4dd94281eeb8f28ed99c0362b4ff58
jiyoung-B <catgirl0313@naver.com>
2023-12-02 14:19:56 +0900

    OnDeleteAction.CASCADE 추가,서비스에 delete 로직 삭제 member 필드 FetchType.LAZY 설정

--- src/main/java/com/example/project3/Entity/Post.java
+++ src/main/java/com/example/project3/Entity/Post.java
@@ -37,13 +37,21 @@ public class Post {
 
 
     @JoinColumn(name = "member_id")
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Member member;
+
+//    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<PostLiked> postLikeds = new ArrayList<>();
+
 
 
     private int countLiked; // 좋아요 수
+//    public int getCountLiked() {
+//        return postLikeds.size();
+//    }
 
-public void setPostHashtags(List<PostHashtag> postHashtags) {
+
+    public void setPostHashtags(List<PostHashtag> postHashtags) {
     this.postHashtags = postHashtags;
 }
 
@@ -51,11 +59,20 @@ public void setPostHashtags(List<PostHashtag> postHashtags) {
     public void increaseCountLiked() {
         countLiked++;
     }
-
+//PostLiked 엔터티를 생성하여 postLikeds 리스트에 추가
+//    public void increaseCountLiked(Member member) {
+//        PostLiked postLiked = PostLiked.builder().post(this).member(member).liked(true).build();
+//        postLikeds.add(postLiked);
+//    }
     // 좋아요 수 감소
     public void decreaseCountLiked() {
         countLiked = Math.max(countLiked - 1, 0);
     }
+// PostLiked 엔터티를 생성하고 해당 엔터티와 일치하는 것을 postLikeds 리스트에서 제거
+//    public void decreaseCountLiked(Member member) {
+//        PostLiked postLiked = PostLiked.builder().post(this).member(member).liked(false).build();
+//        postLikeds.remove(postLiked);
+//    }
 
     @PrePersist // 디비에 INSERT 되기 직전에 실행
     public void createAt() {
@@ -69,6 +86,8 @@ public void setPostHashtags(List<PostHashtag> postHashtags) {
     public void addMediaFile(MediaFile mediaFile) {
         this.mediaFiles.add(mediaFile);
         mediaFile.setPost(this);
+        //mediaFile.setPost(mediaFile.getPost());
+        //mediaFiles.add(mediaFile);
     }
 
     public void update(PostUpdateRequestDto requestDto) {
